7979cf024fb8d1f4d894888f48ca711466696582
Avnish Kumar <avnish@geninf.com>
2019-11-22 18:43:28 +0530

    add migration

---
+++ CoreApiSample/Migrations/20191122130838_CreateDb.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreApiSample.Migrations
+{
+    public partial class CreateDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 255, nullable: true),
+                    Modified = table.Column<DateTimeOffset>(nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    Abbrasive = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 255, nullable: true),
+                    Modified = table.Column<DateTimeOffset>(nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    Abbrasive = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 255, nullable: true),
+                    Modified = table.Column<DateTimeOffset>(nullable: true),
+                    ModifiedBy = table.Column<string>(maxLength: 255, nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    ClassId = table.Column<Guid>(nullable: true),
+                    SubjectId = table.Column<Guid>(nullable: true),
+                    QuestionsText = table.Column<string>(nullable: false),
+                    SolutionText = table.Column<string>(nullable: true),
+                    ImageSolution = table.Column<byte[]>(nullable: true),
+                    ImageMimeType = table.Column<string>(unicode: false, maxLength: 20, nullable: true),
+                    VideoSolutionURL = table.Column<string>(maxLength: 500, nullable: true),
+                    Tag = table.Column<string>(unicode: false, nullable: true),
+                    Writer = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Book = table.Column<string>(unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_160",
+                        column: x => x.ClassId,
+                        principalTable: "Classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_163",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_ClassId",
+                table: "Questions",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_SubjectId",
+                table: "Questions",
+                column: "SubjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}
--- CoreApiSample/Startup.cs
+++ CoreApiSample/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreApiSample.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace CoreApiSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(Configuration.GetConnectionString("Database")));
             services.AddControllers();
         }
 
